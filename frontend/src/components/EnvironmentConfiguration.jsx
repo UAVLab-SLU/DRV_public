6072aac75b28b7f9ed6f51790a9eb894e484b3ef
Szymon <simon.rostkowski@slu.edu>
2023-09-23 15:07:00 -0500

    Commented out envConfid that will be used in another ticket, made the spacing look better

--- frontend/src/components/EnvironmentConfiguration.jsx
+++ frontend/src/components/EnvironmentConfiguration.jsx
@@ -43,7 +43,7 @@ export default function EnvironmentConfiguration (env) {
         Wind: {
             Distance: "NE",
             Force: 1,
-            Type: "None",
+            //Type: "None",
             //WindOrigin: "None",
         },
         Origin: {
@@ -111,7 +111,8 @@ export default function EnvironmentConfiguration (env) {
     ]
 
     const [selectedWindType, setSelectedWindType] = React.useState(
-        envConf.Wind.Type || "None" // Set a default value if needed
+        //envConf.Wind.Type || "None" // Set a default value if needed
+        //"Temp Value"
     );
 
 
@@ -153,6 +154,8 @@ export default function EnvironmentConfiguration (env) {
         }))
     }
 
+
+  // HANDLE WIND ORIGIN
   {/*
   const handleWindOriginChange = (event) => {
         const newWindOrigin = event.target.value;
@@ -168,17 +171,21 @@ export default function EnvironmentConfiguration (env) {
   };
   */}
 
+  
   const handleWindTypeChange = (event) => {
       const newWindType = event.target.value;
-      setSelectedWindType(newWindType);
-      setEnvConf((prevState) => ({
+      setSelectedWindType(newWindType); 
+      // ENV CONFIG
+      {/*setEnvConf((prevState) => ({
           ...prevState,
               Wind: {
               ...prevState.Wind,
               Type: newWindType,
               },
           }));
+      */}
   };
+  
 
     
     const handleDistance = (val) => {
@@ -245,7 +252,8 @@ export default function EnvironmentConfiguration (env) {
                                     </Select>
                                 </FormControl>
                             </Grid>
-                           {/* <Grid item xs={3}> */} {/* WIND ORIGIN DROP DOWN */}
+                            {/* WIND ORIGIN DROP DOWN */}
+                            {/* <Grid item xs={3}> */} 
                                 {/*<FormControl variant="standard" sx={{ minWidth: 150 }}>*/}
                                     {/*<InputLabel id="WindOrigin">Wind Origin</InputLabel>*/}
                                         {/*<Select*/}
@@ -261,12 +269,14 @@ export default function EnvironmentConfiguration (env) {
                                    {/* </Select>*/}
                                 {/*</FormControl>*/}
                             {/*</Grid>*/}
+
+
                             {/* WIND TYPE DROP DOWN */}
 
                             <Tooltip title="Enter Wind Velocity in Meters per second" placement='bottom'>
-                            <Grid item xs={3}>
-                                <TextField id="Force" label="Wind Velocity (m/s)" variant="standard" type="number" onChange={handleWindChange} value={envConf.Wind.Force} disabled={envConf.enableFuzzy}/>
-                            </Grid>
+                                <Grid item xs={3}>
+                                    <TextField id="Force" label="Wind Velocity (m/s)" variant="standard" type="number" onChange={handleWindChange} value={envConf.Wind.Force} disabled={envConf.enableFuzzy}/>
+                                </Grid>
                             </Tooltip>
                             <Grid item xs={3}> 
                                 <FormControl variant="standard" sx={{ minWidth: 150 }}>
@@ -284,9 +294,6 @@ export default function EnvironmentConfiguration (env) {
                                         </Select>
                                 </FormControl>
                             </Grid>
-                            <Tooltip title="Automatically run series of simulations to fuzzy test the wind velocity impact" placement='bottom'>
-
-                            </Tooltip>
                             <Grid item xs={3} />
                             <Grid item xs={3} >
                                 <FormControl variant="standard" sx={{ minWidth: 150 }}>
@@ -306,9 +313,11 @@ export default function EnvironmentConfiguration (env) {
                                  disabled={envConf.Origin.Name=="Specify Region" ? false : true} 
                                  />
                             </Grid>
+
                             <Grid item xs={3}>
                                 <TextField id="Longitude" label="Longitude" variant="standard" type="number" inputProps={{ step: ".0001" }} onChange={handleOriginChange} value={envConf.Origin.Longitude} disabled={envConf.Origin.Name=="Specify Region" ? false : true} />
                             </Grid>
+                            
                             {/*<Grid item xs={3}>*/}
                             {/*    <TextField id="Height" label="Altitude" variant="standard" type="number" inputProps={{ step: "1" }} onChange={handleOriginChange} value={envConf.Origin.Height} disabled={envConf.Origin.Name=="Specify Region" ? false : true}*/}
                             {/*    helperText={envConf.Origin.Name == "Specify Region" ? "Please enter the Altitude above mean sea level. If you're unsure of the exact altitude, please enter 200 as a default value.":  null}/>*/}
@@ -316,17 +325,13 @@ export default function EnvironmentConfiguration (env) {
                             {/* <Grid item xs={3}>
                                 <Typography id="standard-basic" label="Wind">Time of Day</Typography>
                             </Grid> */}
-                        
-                            <Grid item xs={3}>
-                                <FormGroup>
-                                    <FormControlLabel control={<Switch checked={envConf.enableFuzzy} onChange={handleChangeSwitch} inputProps={{ 'aria-label': 'controlled' }} />}  label="Enable Fuzzy Test" />
-                                    <FormHelperText>Please enable this feature if you would like the system to automatically run tests at various wind velocities</FormHelperText>
-                                </FormGroup>
                             </Grid>
-                            <Tooltip title="Enter time of day (24 Hours Format)" placement='bottom'>
-                                <Grid item xs={3}>
-                                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                        <Stack spacing={3}>
+
+                            <Grid container spacing={3} direction="row" sx={{ marginTop: '20px' }}>
+                                <Tooltip title="Enter time of day (24 Hours Format)" placement='bottom'>
+                                    <Grid item xs={3}>
+                                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                            <Stack spacing={3}>
                                             <TimePicker
                                             ampm={false}
                                             openTo="hours"
@@ -339,27 +344,34 @@ export default function EnvironmentConfiguration (env) {
                                             renderInput={(params) => <TextField {...params} 
                                             helperText="Enter Time of Day (24 Hour Format)"/>}
                                             />
-                                        </Stack>
-                                    </LocalizationProvider>
+                                            </Stack>
+                                        </LocalizationProvider>
+                                    </Grid>
+                                </Tooltip>
+                                <Grid item xs={3}>
+                                    <FormGroup>
+                                        <FormControlLabel control={<Switch checked={envConf.enableFuzzy} onChange={handleChangeSwitch} inputProps={{ 'aria-label': 'controlled' }} />}  label="Enable Fuzzy Test" />
+                                        <FormHelperText>Please enable this feature if you would like the system to automatically run tests at various wind velocities</FormHelperText>
+                                    </FormGroup>
                                 </Grid>
-                            </Tooltip>
-                        </Grid>
-
-                        {envConf.Origin.Name == "Specify Region" ? <div style={{width: '100%', height: '450px'}}>
-                            <LoadScript googlMapsApiKey={YOUR_API_KEY}>
-                                <GoogleMap
-                                id="map"
-                                mapContainerStyle={{ height: "100%", width: "100%" }}
-                                zoom={15}
-                                center={{ lat: currentPosition.lat, lng: currentPosition.lng }}
-                                onClick={onMapClick}
-                                >
-                                {currentPosition.lat && currentPosition.lng && (
-                                    <Marker position={{ lat: currentPosition.lat, lng: currentPosition.lng }} />
-                                )}
-                                </GoogleMap>
-                            </LoadScript>
+                            </Grid>
+                            
+                            {envConf.Origin.Name == "Specify Region" ? <div style={{width: '100%', height: '450px'}}>
+                                <LoadScript googlMapsApiKey={YOUR_API_KEY}>
+                                    <GoogleMap
+                                    id="map"
+                                    mapContainerStyle={{ height: "100%", width: "100%" }}
+                                    zoom={15}
+                                    center={{ lat: currentPosition.lat, lng: currentPosition.lng }}
+                                    onClick={onMapClick}
+                                    >
+                                    {currentPosition.lat && currentPosition.lng && (
+                                        <Marker position={{ lat: currentPosition.lat, lng: currentPosition.lng }} />
+                                    )}
+                                    </GoogleMap>
+                                </LoadScript>
                         </div> :null}
+                        
                     </Typography>
                 {/* </Container> */}
             </Box>

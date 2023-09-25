67583a694086a91d674e3e4bb08680b89d411426
Hebron Bekele <105475423+hebronh@users.noreply.github.com>
2023-09-25 16:42:53 -0500

    status

--- frontend/src/const.js
+++ frontend/src/const.js
@@ -2,4 +2,3 @@ export const HOME_LABEL = {
     partfirst: `Simulation of a realistic scenario is an effective way to test the system's requirements.`,
     partsecond: `Describe one or more requirements you would like to test by simulating a scenario`,
   };  
-//export const WIND_TYPES = ["North", "South", "East", "WEST"];

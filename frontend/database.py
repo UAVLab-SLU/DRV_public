625320fbbe5388d25592f7bb69207c90b87f3884
Hebron Bekele <105475423+hebronh@users.noreply.github.com>
2023-10-09 15:44:16 -0500

    database

---
+++ frontend/database.py
@@ -0,0 +1,41 @@
+from dbm import _Database
+import pymongo
+#given airsim api
+
+class Databse:
+
+  def __init__(self):
+    self.client = pymongo.MongoClient()
+    self.db = self.client.airsim_db
+    
+    self.db.defaults.create_index('name', unique=True)
+    self.db.reports.create_index('dates')
+
+  def set_default(self, name, value):
+    self.db.defaults.update_one({'name': name}, {'$set': {'value': value}}, upsert=True)
+# Get drone status
+def get_drone_status(self, name):
+
+  # Call AirSim API to get drone state
+  drone = self.client.getMultirotorState(vehicle_name=name)
+  
+  status = drone.collision.has_collided
+
+  self.db.drones.update_one({'name': name}, {'$set': {'status': status}})
+
+# Set drone speed
+def set_drone_speed(self, name, speed):
+
+  # Call AirSim API to set speed 
+  self.client.moveByVelocityAsync(speed, 0, 0, duration=5, vehicle_name=name)
+  
+  self.db.drones.update_one({'name': name}, {'$set': {'speed': speed}})
+
+# Query drones collection
+for drone in self.db.drones.find():
+  print(drone['name'], drone['status'], drone['speed'])
+
+# Query drones collection
+for drone in self.db.drones.find():
+  print(drone['name'], drone['status'], drone['speed'])
+

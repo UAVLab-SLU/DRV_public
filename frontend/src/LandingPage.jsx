6927f7720ac0fb013dc03ab016b21af0665c0fee
Hebron Bekele <105475423+hebronh@users.noreply.github.com>
2024-04-08 01:54:59 -0500

    updated changes

--- frontend/src/LandingPage.jsx
+++ frontend/src/LandingPage.jsx
@@ -1,22 +1,21 @@
-//import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@mui/styles';
-import Box from '@mui/material/Box'; 
-import Button from '@mui/material/Button'; 
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import ReportDashboard from './components/ReportDashboard';
 
 const useStyles = makeStyles((theme) => ({
   landingPage: {
     fontFamily: 'Roboto, sans-serif',
     color: '#fff',
-    //textAlign: 'center',
-  }, 
+  },
   nav: {
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
     padding: '1.5rem',
-    backgroundColor: '#0000CD', 
+    backgroundColor: '#0000CD',
     fontFamily: 'Arial, sans-serif',
   },
   siteTitle: {
@@ -24,40 +23,55 @@ const useStyles = makeStyles((theme) => ({
     textDecoration: 'none',
     fontSize: '1.5rem',
     fontWeight: 'bold',
-    fontFamily: 'Arial, sans-serif', 
+    fontFamily: 'Arial, sans-serif',
   },
   mainContent: {
     padding: '2rem',
   },
-  buttonContainer: {  
-    position: 'absolute',
-    marginTop: '5rem', 
-    top: '1rem', 
-  },   
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    marginTop: '1rem',
+    marginRight: '1.6rem',
+    top: '1rem'
+  },
   navList: {
-    listStyleType: 'none', 
+    listStyleType: 'none',
     margin: 0,
     padding: 0,
-    fontFamily: 'Arial, sans-serif', 
+    fontFamily: 'Arial, sans-serif',
   },
   navListItem: {
     display: 'inline-block',
     marginLeft: '1rem',
   },
-  
   aboutLink: {
     textDecoration: 'none',
     color: '#fff',
     padding: '0.5rem 1rem',
-    borderRadius: '30px', 
-   // backgroundColor: 'rgba(255,255,255,0.2)', 
-    transition: 'background-color 0.3s ease', 
-   // backgroundColor: '#87CEEB', 
+    borderRadius: '30px',
   },
 }));
 
 export default function LandingPage() {
   const classes = useStyles();
+  const [filesPresent, setFilesPresent] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/list-reports', { method: 'GET' });
+      
+        const data = await response.json();
+        const batchFiles = data.reports.filter(file => file.filename.includes('Batch'));
+        setFilesPresent(batchFiles.length > 0);
+      } catch (error) {
+        console.error('Error fetching report data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className={classes.landingPage}>
@@ -75,39 +89,33 @@ export default function LandingPage() {
           </li>
         </ul>
       </nav>
-      {/* Main content area */}
-      <div className={classes.mainContent} style={{paddingTop: '9rem', color: '#333'}}>
-       
-        <div className={classes.buttonContainer} style={{ color: '#333', position: 'right'}}> 
-        <Link to="/">
+      <div className={classes.mainContent}>
+        <div className={classes.buttonContainer}>
+          <Link to="/">
             <Button
-             variant="contained"
+              variant="contained"
               sx={{
-                //backgroundColor: '#ADD8E6',
                 color: 'white',
                 padding: '15px 30px',
-                borderRadius: '10px', 
-                textAlign: 'right' 
-                //right: '15rem'
-                //backgroundColor: '#87CEEB',
+                borderRadius: '10px',
               }}
-            > 
+            >
               Create Simulation
             </Button>
-          </Link> 
-          <h1>Report History:</h1> 
-
-          <Link to= "/report-dashboard">
-            <div
-             
-            >  
-             {/* Contents of the LandingPage */}
-            {/* Embed the ReportDashboard component */}
-            <ReportDashboard />
-           
-            </div> 
           </Link>
         </div>
+        <h1 style={{ marginTop: '3rem' }}></h1>
+        {filesPresent ? (
+          <Link to="/report-dashboard">
+            <div>
+              <ReportDashboard />
+            </div>
+          </Link>
+        ) : (
+          <div style={{ textAlign: 'center' }}>
+            <h2>No batch files present in the report dashboard</h2>
+          </div>
+        )}
       </div>
     </div>
   );

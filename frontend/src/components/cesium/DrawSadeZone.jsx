25c80420d915b17067099d99f88093d6a7178c35
anjaliputta <aputta2@slu.edu>
2024-06-27 23:49:14 -0500

    add multiple sade zones

--- frontend/src/components/cesium/DrawSadeZone.jsx
+++ frontend/src/components/cesium/DrawSadeZone.jsx
@@ -1,14 +1,16 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { Entity } from 'resium';
-import { ScreenSpaceEventType, Cartographic, Color, Rectangle, KeyboardEventModifier, 
-    ScreenSpaceEventHandler, Ellipsoid, defined } from 'cesium';
+import {
+  ScreenSpaceEventType, Cartographic, Color, Rectangle, KeyboardEventModifier,
+  ScreenSpaceEventHandler, Ellipsoid, Math as CesiumMath, defined
+} from 'cesium';
 import PropTypes from 'prop-types';
 
 const DrawSadeZone = ({ viewerReady, viewerRef, setNewCameraPosition }) => {
   const [mouseDown, setMouseDown] = useState(false);
   const [firstPoint, setFirstPoint] = useState(null);
-  const [rectangleCoordinates, setRectangleCoordinates] = useState(null);
-  const [showSelector, setShowSelector] = useState(false);
+  const [sadeZones, setSadeZones] = useState([]);
+  const [selectedZoneIndex, setSelectedZoneIndex] = useState(null);
   const [isEditing, setIsEditing] = useState(false);
   const [selectedPoint, setSelectedPoint] = useState(null);
   const [corners, setCorners] = useState({});
@@ -27,15 +29,28 @@ const DrawSadeZone = ({ viewerReady, viewerRef, setNewCameraPosition }) => {
         const tempCartographic = Cartographic.fromCartesian(cartesian, Ellipsoid.WGS84);
         if (!firstPoint) {
           setFirstPoint(tempCartographic);
+          setSadeZones(prevSadeZones => [
+            ...prevSadeZones,
+            new Rectangle(
+              tempCartographic.longitude, tempCartographic.latitude,
+              tempCartographic.longitude, tempCartographic.latitude
+            )
+          ]);
         } else {
-          const newRectangle = new Rectangle(
-            Math.min(tempCartographic.longitude, firstPoint.longitude),
-            Math.min(tempCartographic.latitude, firstPoint.latitude),
-            Math.max(tempCartographic.longitude, firstPoint.longitude),
-            Math.max(tempCartographic.latitude, firstPoint.latitude)
-          );
-          setRectangleCoordinates(newRectangle);
-          setShowSelector(true);
+          setSadeZones(prevSadeZones => {
+            const updatedZones = [...prevSadeZones];
+            const lastRectangleIndex = updatedZones.length - 1;
+
+            updatedZones[lastRectangleIndex] = new Rectangle(
+              Math.min(tempCartographic.longitude, firstPoint.longitude),
+              Math.min(tempCartographic.latitude, firstPoint.latitude),
+              Math.max(tempCartographic.longitude, firstPoint.longitude),
+              Math.max(tempCartographic.latitude, firstPoint.latitude)
+            );
+
+            return updatedZones;
+          });
+
           setNewCameraPosition();
         }
       }
@@ -48,37 +63,37 @@ const DrawSadeZone = ({ viewerReady, viewerRef, setNewCameraPosition }) => {
     handler.setInputAction(() => {
       setMouseDown(false);
       setFirstPoint(null);
-      setShowSelector(false);
       setNewCameraPosition();
-    }, ScreenSpaceEventType.LEFT_UP, KeyboardEventModifier.SHIFT);
+    }, ScreenSpaceEventType.LEFT_UP);
 
     return () => {
       handler.destroy();
     };
-  }, [viewerReady, mouseDown, firstPoint, rectangleCoordinates, showSelector]);
+  }, [viewerReady, mouseDown, firstPoint, sadeZones]);
 
   return (
     <>
-    {showSelector &&
-    <Entity
-    rectangle={{
-    coordinates: rectangleCoordinates,
-    material: Color.GREEN.withAlpha(0.5),
-    outline: true,
-    outlineColor: Color.WHITE,
-    outlineWidth: 2,
-    extrudedHeight: 30.0,
-    }}
-    />
-    }
+      {sadeZones.map((sadeZone, index) => (
+        <Entity
+          key={index}
+          rectangle={{
+            coordinates: sadeZone,
+            material: Color.GREEN.withAlpha(0.5),
+            outline: true,
+            outlineColor: Color.WHITE,
+            outlineWidth: 2,
+            extrudedHeight: 30.0,
+          }}
+        />
+      ))}
     </>
   );
 };
 
 DrawSadeZone.propTypes = {
-    viewerReady: PropTypes.bool.isRequired,
-    viewerRef: PropTypes.object.isRequired,
-    setNewCameraPosition: PropTypes.func.isRequired
+  viewerReady: PropTypes.bool.isRequired,
+  viewerRef: PropTypes.object.isRequired,
+  setNewCameraPosition: PropTypes.func.isRequired
 };
 
 export default DrawSadeZone;
